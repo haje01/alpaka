502ea20963bd5d0ef2fc598ee91f916223a5d83f
JeongJu Kim <haje01@gmail.com>
2022-11-29 14:45:44 +0900

    cli 에서 kfktest 이용

--- images/build_cli.sh
+++ images/build_cli.sh
@@ -4,5 +4,6 @@ version=`cat VERSION.cli`
 
 docker build -t $USERNAME/$IMAGE:latest -f Dockerfile.cli .
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+docker login -u haje01
 docker push $USERNAME/$IMAGE:latest
 docker push $USERNAME/$IMAGE:$version
