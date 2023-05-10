6c29bbff11d08ebf932d433845cacfb1cdf8e2de
JeongJu Kim <haje01@gmail.com>
2023-05-10 18:00:00 +0900

    fix username

--- images/build_tool.sh
+++ images/build_tool.sh
@@ -4,6 +4,6 @@ version=`cat VERSION.tool`
 
 docker build -t $USERNAME/$IMAGE:latest -f Dockerfile.tool .
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
-docker login -u haje01
+# docker login -u $USERNAME
 docker push $USERNAME/$IMAGE:latest
 docker push $USERNAME/$IMAGE:$version
